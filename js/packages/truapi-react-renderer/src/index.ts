892e52088872c7dd391cba2a1e6c8202e0cc72f8
Eugenio Paluello <eugenio@parity.io>
2026-08-04 18:21:47 +0200

    feat: port React reconciler and renderer entry point

---
+++ js/packages/truapi-react-renderer/src/index.ts
@@ -0,0 +1,14 @@
+export {
+  Box,
+  Button,
+  Column,
+  Row,
+  Spacer,
+  Text,
+  TextField,
+} from "./components.js";
+
+export type { CustomRendererNode } from "@parity/truapi";
+
+// React renderer
+export { createRenderer } from "./renderer.js";
