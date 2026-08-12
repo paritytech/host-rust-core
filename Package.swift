b1a7afe05fc0f8e765339895b2fca5aa5cda173e
ERussel <emkil.russel@gmail.com>
2026-08-12 10:37:50 +0200

    update ios library to 0.5.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ import PackageDescription
 // The published release asset remains the default for remote consumers.
 let useLocalBinary = ProcessInfo.processInfo.environment["TRUAPI_USE_LOCAL_BINARY"] == "1"
 
-let publishedBinaryURL = "https://github.com/paritytech/truapi/releases/download/%40parity%2Fios-host%400.4.0-chat-modality-shared-core.4/truapi_server.xcframework.zip"
-let publishedBinaryChecksum = "b4d7e633e8a86bdaf104a1bcafc883549fc893514451fe7caeb951107e5038f7"
+let publishedBinaryURL = "https://github.com/paritytech/truapi/releases/download/%40parity%2Fios-host%400.5.0/truapi_server.xcframework.zip"
+let publishedBinaryChecksum = "3702499b6380f7d5de2a887d9d1704c8c4785030d8850b704f6b17bd0e26a7ed"
 
 let binaryTarget: Target = useLocalBinary
     ? .binaryTarget(
